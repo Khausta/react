0bc8b8d0502e346d7073a9df7155f37a16451146
Khausta <anna.a.khaustova@gmail.com>
2023-11-23 21:18:48 +0300

    react fragment and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
 import './App.css';
 
 function App() {
 
+  // return React.createElement('div', {}, 'Project New 2023');
+
   return (
-    <div>Project</div>
+    // <React.Fragment></React.Fragment> --- добавляем тк нуден родительский элемент. но можем заменить <></>
+    <>
+      <h1>Header</h1>
+      <p>Paragraph</p>
+    </>
   );
 }
 
