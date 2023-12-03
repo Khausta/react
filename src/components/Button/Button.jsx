f9e4d21e10617ed2d08077665938805565e0858d
Khausta <anna.a.khaustova@gmail.com>
2023-12-03 14:44:01 +0300

    useState

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,18 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
-
+	const [text, setText] = useState('Save');
+	console.log('Rerender');
 	const clicked = () => { 
+		setText('Close');
+		setText(t => t + '!');
 		console.log('Hello!');
+		console.log(text);
 	};
 
 	return (
-		<button onClick={clicked} className='button accent'>Save</button>
+		<button onClick={clicked} className='button accent'>{text}</button>
 	);
 }
   
