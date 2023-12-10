868524602f46dd549b6da5f1ba630aecf9603226
Khausta <anna.a.khaustova@gmail.com>
2023-12-10 20:52:50 +0300

    Elements adding

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,33 @@ import JournalList from './components/JournalList/JournalList';
 import Header from './components/Header/Header';
 import Body from './layouts/Body/Body';
 import JournalForm from './components/JournalForm/JournalForm';
-import Button from './components/Button/Button';
+import { useState } from 'react';
+
+const INITIAL_DATA = [
+	{
+		title: 'Подготовка к обновлению курсов',
+		text: 'Горные походы открывают удивительные природные ландшафты, испытывают туристов физически и морально, дают возможность почувствовать себя первопроходцем. У горных походов свои секреты, приобщиться к которым можно только в команде единомышленников и профессионалов.',
+		date: new Date()
+	},
+	{
+		title: 'Поход в горы',
+		text: 'Различают альпинизм и горный туризм. Если в альпинизме главная цель – покорение вершины, то горный туризм – это длительное путешествие в горах, связанное с прохождением многочисленных препятствий. В нем огромную роль играет физическая подготовка путешественников, их выносливость, способность переносить большие нагрузки и тяжести в условиях высокогорья.',
+		date: new Date()
+	}
+];
 
 function App() {
 
-	const data = [
-		{
-			title: 'Подготовка к обновлению курсов',
-			text: 'Горные походы открывают удивительные природные ландшафты, испытывают туристов физически и морально, дают возможность почувствовать себя первопроходцем. У горных походов свои секреты, приобщиться к которым можно только в команде единомышленников и профессионалов.',
-			date: new Date()
-		},
-		{
-			title: 'Поход в горы',
-			text: 'Различают альпинизм и горный туризм. Если в альпинизме главная цель – покорение вершины, то горный туризм – это длительное путешествие в горах, связанное с прохождением многочисленных препятствий. В нем огромную роль играет физическая подготовка путешественников, их выносливость, способность переносить большие нагрузки и тяжести в условиях высокогорья.',
-			date: new Date()
-		}
-	];
+	const [items, setItems] = useState(INITIAL_DATA);
+
+	const addItem = item => {
+		setItems(oldItems => [...oldItems, {
+			text: item.text,
+			title: item.title,
+			date: new Date(item.date)
+		}]);
+	};
+	
 
 	return (
 		<div className='app'>
@@ -31,8 +42,7 @@ function App() {
 				<JournalItemAddButton>
 				</JournalItemAddButton>
 				<JournalList>
-					{/* {[<Button>1</Button>, <Button>2</Button>]} */}
-					{data.map(el => (
+					{items.map(el => (
 						<CardButton>
 							<JournalItem
 								title={el.title}
@@ -44,7 +54,7 @@ function App() {
 				</JournalList>
 			</LeftPanel>
 			<Body>
-				<JournalForm/>
+				<JournalForm onSubmit={addItem} />
 			</Body>
 		</div>
 	);
