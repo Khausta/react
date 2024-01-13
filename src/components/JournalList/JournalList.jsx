e7ed638401fa993fc6c720f3e14b08ae8389fc9c
Khausta <anna.a.khaustova@gmail.com>
2024-01-13 14:11:49 +0300

    useMemo

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,17 +1,13 @@
 import './JournalList.css';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { UserContext } from '../../context/user.context';
 
 
 function JournalList({ items }) {
 
 	const { userId } = useContext(UserContext);
-
-	if (items.length === 0) {
-		return <p>Записей нет</p>;
-	}
 	
 	const sortItems = (a, b) => {
 		if (a.date < b.date) {
@@ -21,12 +17,20 @@ function JournalList({ items }) {
 		}
 	};
 
+	const filtredItems = useMemo(() => items
+		.filter(el => el.userId === userId)
+		.sort(sortItems), [items, userId]); 
+
+	
+
+	if (items.length === 0) {
+		return <p>Записей нет</p>;
+	}
+	
 	
 
 	return <>
-		{items
-			.filter(el => el.userId === userId)
-			.sort(sortItems)
+		{filtredItems
 			.map(el => (
 				<CardButton key={el.id}>
 					<JournalItem
